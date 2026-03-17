f799cbb09a2b4b183ef6496f7ada963bac1603d4
xinlin4 <xinlinchenzhang@gmail.com>
2026-03-17 21:32:58 +0100

    Main components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Skills } from './pages/skills/skills';
+import { Projects } from './pages/projects/projects';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: '/skills', component: Skills},
+    {path: '/project', component: Projects},
+    {path: '/contact', component: Contact}
+];
