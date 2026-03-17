376ff6f4ff529ccea417494ac235eafbb2a1fbab
xinlin4 <xinlinchenzhang@gmail.com>
2026-03-17 22:34:12 +0100

    Projects added

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { App } from './app/app';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(App, {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient()]
 });
